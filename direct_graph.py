191d64be72d63e8d8f046a07359129792e56f76e
RilaKorila <murakami.ayana3@gmail.com>
2023-10-05 23:42:05 +0900

    wip: 有向グラフの型を追加(set_edge_weight)

--- direct_graph.py
+++ direct_graph.py
@@ -7,17 +7,23 @@ class Path:
         self.path = path
 
 class DirectGraph:
-    def __init__(self, nodes, paths):
+    def __init__(self, nodes, edges, paths):
         self.nodes = nodes
+        self.edges = edges
         self.paths = paths
     
     def calc_edge_weight(self):
-        edge_weight = defaultdict(int)
+        self.edge_weight = defaultdict(int)
 
         for path in self.paths:
             for node_id1, node_id2 in zip(path[:-1], path[1:]):
                 edge_id = self.get_edge_id(node_id1, node_id2)
-                edge_weight[edge_id] += 1
+                self.edge_weight[edge_id] += 1
+
+    def set_edge_weight(self):
+        for edge in self.edges:
+            edge.weight = self.edge_weight[edge.id]
+
 
     def get_edge_id(node_id1, node_id2):
         return 1
