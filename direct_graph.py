5fa4a7c3ce1cb8d9cc5fc66d9f26e017bc4d5300
RilaKorila <murakami.ayana3@gmail.com>
2023-10-07 00:30:15 +0900

    有向グラフのためのDirect_Edgeクラスを定義

--- direct_graph.py
+++ direct_graph.py
@@ -5,20 +5,50 @@ from graph import Edge, Node
 
 class Path:
     def __init__(self, path_id, path):
+        """
+        path_id: 経路固有のid
+        path: node_idのリスト
+        """
         self.path_id = path_id
         self.path = path
 
 
+class DirectEdge:
+    def __init__(self, id, node1, node2):
+        self.id = int(id)
+        self.node_from = int(node1)
+        self.node_to = int(node2)
+        self.weight = 0
+
+    def set_weight(self, weight):
+        self.weight = weight
+
+
+# NodeとPat
 class DirectGraph:
     def __init__(self, nodes, edges, paths):
         self.nodes = nodes
-        self.edges = edges
         self.paths = paths
+        self.direct_edges = self.create_direct_edges()
         self.edge_id_dict = {}
 
-        for e in list(self.edges):
-            pair = (e.node1, e.node2) if e.node1 > e.node2 else (e.node2, e.node1)
-            self.edge_id_dict[pair] = e.id
+        # TODO; create_direct_edgesの中での処理を検討
+        for e in list(self.direct_edges):
+            self.edge_id_dict[(e.node_from, e.node_to)] = e.id
+
+    def create_direct_edges(self):
+        edge_candidates = set()
+        direct_edges = set()
+        for path in self.paths:
+            for node1_id, node2_id in zip(path[:-1], path[1:]):
+                # 有向グラフなので、(node1_id, node2_id)と(node2_id, node1_id)は別物
+                edge_candidates.add((node1_id, node2_id))
+
+        for id, (node_from, node_to) in enumerate(list(edge_candidates)):
+            direct_edge = DirectEdge(id, node_from, node_to)
+            direct_edges.add(direct_edge)
+
+        return direct_edges
 
     def calc_edge_weight(self):
         self.edge_weight = defaultdict(int)
@@ -30,4 +60,4 @@ class DirectGraph:
 
     def set_edge_weight(self):
         for edge in self.edges:
-            edge.weight = self.edge_weight[edge.id]
+            edge.set_weight(self.edge_weight[edge.id])
