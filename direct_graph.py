1e0259605f6f9e9eb15a6bc5b646f04253d708c5
RilaKorila <murakami.ayana3@gmail.com>
2023-10-06 23:37:24 +0900

    edge両端のnodeのペアからedge_idを特定

--- direct_graph.py
+++ direct_graph.py
@@ -11,21 +11,20 @@ class DirectGraph:
         self.nodes = nodes
         self.edges = edges
         self.paths = paths
+        self.edge_id_dict = {}
+
+        for e in list(self.edges):
+            pair = (e.node1, e.node2) if e.node1 >  e.node2 else (e.node2, e.node1)
+            self.edge_id_dict[pair] = e.id
     
     def calc_edge_weight(self):
         self.edge_weight = defaultdict(int)
 
         for path in self.paths:
             for node_id1, node_id2 in zip(path[:-1], path[1:]):
-                edge_id = self.get_edge_id(node_id1, node_id2)
+                edge_id = self.edge_id_dict((node_id1, node_id2))
                 self.edge_weight[edge_id] += 1
 
     def set_edge_weight(self):
         for edge in self.edges:
             edge.weight = self.edge_weight[edge.id]
-
-
-    def get_edge_id(node_id1, node_id2):
-        return 1
-
-        
